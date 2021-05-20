87de45acbe0fcef66585a07f44bd9c9385ade24d
Rohaan Rehman <>
2021-05-20 00:35:12 +0000

    added button

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from tkinter import *
 window = Tk()
 
+
+btn=Button(window, text="Continue", fg='black', bg='cyan')
+btn.place(x=275, y=100)
+lbl=Label(window, text="Global Pollution", fg='cyan', font=("Helvetica", 18))
+lbl.place(x=220, y=50)
+
 window.title('Quiz on Global pollution')
-window.geometry("500x600+10+20")
+window.geometry("650x400+10+20")
+window.mainloop()
