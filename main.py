a74449f29ae13140701ea1de851532314fce8eed
Rohaan Rehman <>
2021-05-20 08:23:31 +0000

    deleted everything and made a class and add background color

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 from tkinter import *
 window = Tk()
 
+class main_screen:
+    def __innit__(self,parent):
 
-btn=Button(window, text="Continue", fg='black', bg='cyan')
-btn.place(x=275, y=100)
-lbl=Label(window, text="Global Pollution", fg='cyan', font=("Helvetica", 18))
-lbl.place(x=220, y=50)
-
-window.title('Quiz on Global pollution')
-window.geometry("650x400+10+20")
-window.mainloop()
+        background_color="black"
+        
